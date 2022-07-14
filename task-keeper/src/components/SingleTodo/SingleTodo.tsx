012f295055e837d0b22684353bcd3f45df6a1072
Santosh <s.kunga.ganeshbabu@accenture.com>
2022-07-14 15:32:03 +0530

    edit functionality added, delete, done updated

--- task-keeper/src/components/SingleTodo/SingleTodo.tsx
+++ task-keeper/src/components/SingleTodo/SingleTodo.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
+import "./SingleTodo.css"
 import { Task } from '../InputField/Model/InputFieldProps'
 import { DeleteOutlined, ModeEditOutline, Done, RemoveCircleOutline } from '@mui/icons-material';
 
@@ -10,6 +11,13 @@ interface Props {
 
 const SingleTodo: React.FC<Props> = ({ todo, todos, setTodos }) => {
 
+    const [isEditable, setIsEditable] = useState<boolean>(false);
+    const [editableTodo, setEditableTodo] = useState<string>(todo.todo)
+
+    const enableEdit = () => {
+        setIsEditable(true);
+    }
+
     const handleDone = (id: number) => {
         setTodos(todos.map((todo) =>
             todo.id === id ? { ...todo, isDone: !todo.isDone } : todo
@@ -17,23 +25,62 @@ const SingleTodo: React.FC<Props> = ({ todo, todos, setTodos }) => {
     }
 
     const handleDelete = (id: number) => {
-        setTodos(todos.filter(todo => todo.id!==id));
+        isEditable ? alert("complete the edit first") :
+            setTodos(todos.filter(todo => todo.id !== id));
     }
+
+    const handleEdit = (e: React.FormEvent<HTMLFormElement>, id: number) => {
+        e.preventDefault();
+        setTodos(todos.map((todo) =>
+            (todo.id === id) ? { ...todo, todo: editableTodo } : todo
+        ))
+        setIsEditable(false);
+    }
+
+    const todoEditInputRef = useRef<HTMLInputElement>(null);
 
     return (
         <li className={`todo-item ${todo.isDone ? "task-done" : ''}`} key={todo.id} >
-            {todo.todo}
+            <form onSubmit={(e) => {
+                handleEdit(e, todo.id);
+                todoEditInputRef.current?.blur();
+            }}>
+                {
+                    isEditable ? (
+                        <div className='edit-container'>
+                            <input
+                                className='editableInput'
+                                ref={todoEditInputRef}
+                                type="text"
+                                name="todo"
+                                value={editableTodo}
+                                onChange={(e) => setEditableTodo(e.target.value)} />
+                            <button className='icon edit-done-button'>
+                                <Done className='edit-done-icon' />
+                            </button>
+                        </div>
+                    ) : (
+                        todo.todo
+                    )
+                }
+
+            </form>
             <span className="item-actions">
-                <span className="icon edit">
-                    <ModeEditOutline />
-                </span>
-                <span className="icon delete" onClick={()=>handleDelete(todo.id)}>
+                {
+                    (!isEditable && !todo.isDone) ? (
+                        <span className="icon edit" onClick={() => enableEdit()}>
+                            <ModeEditOutline />
+                        </span>
+                    ) : ''
+                }
+
+                <span className="icon delete" onClick={() => handleDelete(todo.id)}>
                     <DeleteOutlined />
                 </span>
                 {
-                    todo.isDone ? (
+                    isEditable ? '' : todo.isDone ? (
                         < span className="icon done" onClick={() => handleDone(todo.id)}>
-                            <RemoveCircleOutline  />
+                            <RemoveCircleOutline />
                         </span >
                     ) : (
                         < span className="icon" onClick={() => handleDone(todo.id)}>
