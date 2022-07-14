2ef3922a1217ccdb3010da82e5a9fd4e9ae5e58f
Santosh <s.kunga.ganeshbabu@accenture.com>
2022-07-14 10:23:44 +0530

    done and delete function, conditional isdone

--- task-keeper/src/components/SingleTodo/SingleTodo.tsx
+++ task-keeper/src/components/SingleTodo/SingleTodo.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Task } from '../InputField/Model/InputFieldProps'
-import { DeleteOutlined, ModeEditOutline, Done } from '@mui/icons-material';
+import { DeleteOutlined, ModeEditOutline, Done, RemoveCircleOutline } from '@mui/icons-material';
 
 interface Props {
     todo: Task
@@ -10,10 +10,15 @@ interface Props {
 
 const SingleTodo: React.FC<Props> = ({ todo, todos, setTodos }) => {
 
-    const tickButton = todo.isDone ? '' :
-        (< span className="icon done" >
-            <Done />
-        </span >);
+    const handleDone = (id: number) => {
+        setTodos(todos.map((todo) =>
+            todo.id === id ? { ...todo, isDone: !todo.isDone } : todo
+        ))
+    }
+
+    const handleDelete = (id: number) => {
+        setTodos(todos.filter(todo => todo.id!==id));
+    }
 
     return (
         <li className={`todo-item ${todo.isDone ? "task-done" : ''}`} key={todo.id} >
@@ -22,10 +27,20 @@ const SingleTodo: React.FC<Props> = ({ todo, todos, setTodos }) => {
                 <span className="icon edit">
                     <ModeEditOutline />
                 </span>
-                <span className="icon delete">
+                <span className="icon delete" onClick={()=>handleDelete(todo.id)}>
                     <DeleteOutlined />
                 </span>
-                {tickButton}
+                {
+                    todo.isDone ? (
+                        < span className="icon done" onClick={() => handleDone(todo.id)}>
+                            <RemoveCircleOutline  />
+                        </span >
+                    ) : (
+                        < span className="icon" onClick={() => handleDone(todo.id)}>
+                            <Done />
+                        </span >
+                    )
+                }
             </span>
         </li>
     )
